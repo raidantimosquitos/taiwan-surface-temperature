7cf46042ba3909e2939cc502fb306be51b6872f6
raidantimosquitos <raidantimosquitos@github.com>
2024-11-11 16:13:16 +0800

    Fixed coordinates and names on Kaggle dataset

--- data-summary/main.py
+++ data-summary/main.py
@@ -6,15 +6,33 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 
 d = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-datasets_dir = d + '/datasets/berkeley-earth-surface-temp-dataset/'
+datasets_dir = d + '/datasets/'
+output_dir = d + '/data-summary/'
 
 # adjusting display options to make sure all columns are visible and up to 100 rows can be displayed
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 50)
 pd.set_option('display.max_colwidth', None)
 
-df_cities = pd.read_csv(datasets_dir + 'GlobalLandTemperaturesByCity.csv')
+df_cities = pd.read_csv(datasets_dir + 'berkeley-earth-surface-temp-dataset/GlobalLandTemperaturesByCity.csv')
+
+# dataframe with corrected latitude-longitude values and city names
+df_coords = pd.read_csv(datasets_dir + 'worldcities-coordinates/taiwan-city-names-and-coordinates.csv')
 df_tw = df_cities[df_cities['Country'] == 'Taiwan']
+
+# assign correct coordinates and city names to df_tw dataframe
+for index, row in df_coords.iterrows():
+    city_name = row['City']
+    corrected_cn = row['Name-equivalent']
+    lat = row['Latitude']
+    long = row['Longitude']
+
+    # changing coordinates
+    df_tw.loc[df_tw['City'] == city_name, 'Latitude'] = lat
+    df_tw.loc[df_tw['City'] == city_name, 'Longitude'] = long
+    df_tw.loc[df_tw['City'] == city_name, 'City'] = corrected_cn
+
+df_tw = df_tw.reset_index(drop=True)
 
 # preview of the dataset
 print(df_tw)
@@ -37,6 +55,9 @@ print('Duplicates: ', df_tw.duplicated().sum())
 # print description of dataset
 print(df_tw.describe().T)
 
+# output the corrected dataset
+df_tw.to_csv(output_dir + 'TaiwanLandTemperaturesByCity.csv')
+
 # After all above we can observe that our dataset has 30 cities/regions in 
 # Taiwan with total 62106 samples, no duplicates and 84 missing values in 
 # average temperature and average temperature uncertainty features.
@@ -51,12 +72,9 @@ for city in df_aux['City'].values:
     average_temp.append(mean_temp.astype(np.float32))
 
 df_aux['AverageTemperature'] = average_temp
-
-df_aux['Latitude'] = df_aux['Latitude'].str.replace(r'N', '', regex=True)
 df_aux['Latitude'] = df_aux['Latitude'].astype(np.float32)
-
-df_aux['Longitude'] = df_aux['Longitude'].str.replace(r'E', '', regex=True)
 df_aux['Longitude'] = df_aux['Longitude'].astype(np.float32)
+
 df_aux = df_aux.reset_index(drop=True)
 
 print()
@@ -82,35 +100,11 @@ cbar = plt.colorbar(scatter, ax=ax)
 cbar.set_label('Average Temperature (°C)')
 
 # Set axis limits to focus on Taiwan
-ax.set_xlim(119, 123)
-ax.set_ylim(23, 25)
+ax.set_xlim(120, 122)
+ax.set_ylim(22, 25.5)
 ax.set_title('Average Temperatures in Taiwanese Cities')
 
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
 plt.grid(True)
 plt.show()
-
-# From the plot you can appreciate how the coordinates are clustered in four groups,
-# this is because in the original dataset there were only two decimal digit accuracy 
-# to specify latitude and longitude. In the end merging the cities that are close to
-# each other in a small country as Taiwan is.
-
-df_aux['Group'] = df_aux.groupby(['Latitude', 'Longitude']).ngroup()
-
-print()
-print('Group 0: South West cities')
-print(df_aux.loc[df_aux['Group'] == 0])
-
-print('Group 1: South East cities')
-print(df_aux.loc[df_aux['Group'] == 1])
-
-print('Group 2: Central cities')
-print(df_aux.loc[df_aux['Group'] == 2])
-
-print('Group 3: North cities')
-print(df_aux.loc[df_aux['Group'] == 3])
-
-# However after checking on Google Maps, it seems something is wrong with the
-# coordinates in the dataset, cities like Nantou and Taitung should not be in
-# the same group.
